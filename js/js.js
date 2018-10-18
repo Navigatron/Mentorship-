f5a527989383b7060bb0c632d7448e61b612cc92
Ethan Witherington <wyddindustries@gmail.com>
2018-10-18 19:04:46 -0400

    push

--- js/js.js
+++ js/js.js
@@ -8,27 +8,42 @@ var ciphertextbox;
 var cleartextbox;
 
 var init = function(){
+	// Initialize Ciphertext Box
 	ciphertextbox = document.getElementById('ciphertext');
-	// This method of adding listeners does not work
-	// ciphertextbox.addEventListener('onchange', OnStateChange);
-	// ciphertextbox.addEventListener('onkeyup', OnStateChange);
-	// ciphertextbox.addEventListener('onpaste', OnStateChange);
-	// ciphertextbox.addEventListener('oninput', OnStateChange);
 	ciphertextbox.onchange = OnStateChange;
 	ciphertextbox.onkeyup = OnStateChange;
 	ciphertextbox.onpaste = OnStateChange;
 	ciphertextbox.oninput = OnStateChange;
+
+	// Initialize the Cleartext Box
 	cleartextbox = document.getElementById('cleartext');
 };
 
 var OnStateChange = function(){
-	console.log("HEY");
+	console.log("Handling Input");
+	// Step One - Get Input Text
+	let text = getInputText();
+	// Step Two - do LFA on text
+
+	// Step Three - Match text LFA to reference LFA
+
+	// Step Four - Translate text
+
+	// Step Five - publish results
+	showResult(text);
+	/*
+	Step 1 - Pull text from input
+	Step 2 - Perform LFA on input
+	Step 3 - Determine Translation Alphabet
+	Step 4 - Perform Translation
+	Step 5 - Show result in output
+	*/
 };
 
-/*
-Step 1 - Pull text from input
-Step 2 - Perform LFA on input
-Step 3 - Determine Translation Alphabet
-Step 4 - Perform Translation
-Step 5 - Show result in output
-*/
+var showResult = function(text) {
+	cleartextbox.value = text;
+}
+
+var getInputText = function(){
+	return ciphertextbox.value;
+}
